652aa302ded06d0b935a5a4e4d3e550fd360bead
Kara Lee Carver <karasactualemail@gmail.com>
2024-09-16 15:49:32 -0600

    Did the assignments.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,52 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What was your grade? ");
+        string gradeStr = Console.ReadLine();
+        int grade = int.Parse(gradeStr);
+        string letterGrade = "N/A";
+
+        if (grade < 60)
+        {
+            letterGrade = "F";
+        }
+        else if (grade >= 60 && grade < 70)
+        {
+            letterGrade = "D";
+        }
+        else if (grade >= 70 && grade < 80)
+        {
+            letterGrade = "C";
+        }
+        else if (grade >= 80 && grade < 90)
+        {
+            letterGrade = "B";
+        }
+        else if (grade >= 90)
+        {
+            letterGrade = "A";
+        }
+
+        if (grade % 10 >= 7)
+        {
+            if (letterGrade != "A" && letterGrade != "F")
+            {
+                letterGrade = $"{letterGrade}+";
+            }
+        }
+        else if (grade % 10 <= 3)
+        {
+            if (letterGrade != "A" && letterGrade != "F")
+            {
+                letterGrade = $"{letterGrade}-";
+            }
+        }
+
+        Console.WriteLine($"Your grade was a {letterGrade}.");
+
     }
 }
