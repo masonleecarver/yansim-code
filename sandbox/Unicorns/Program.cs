e7d10f1440718a155b42cede909a574ee0a9c51b
Kara Lee Carver <karasactualemail@gmail.com>
2024-09-30 15:07:42 -0600

    i actually don't rememeber what i did

--- sandbox/Unicorns/Program.cs
+++ sandbox/Unicorns/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("My stomach friggin hurts!!!!!!!!!!!!!!!!!!!!!");
+
+        Random rand = new();
+        int d1 = rand.Next(1,7);
+        int d2 = rand.Next(1,7);
+        int d3 = rand.Next(1,7);
+
+        List <int> diceList = [d1, d2, d3];
+        diceList = diceList.Sort();
+
+        foreach (int die in diceList)
+        {
+            Console.WriteLine($"{die}");
+        } 
+
     }
 }
