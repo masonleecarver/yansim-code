491542267a85e88df9f6da3eff26a3c660106beb
Kara Lee Carver <karasactualemail@gmail.com>
2024-10-28 12:10:29 -0600

    completed the prepare activity for week 7

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -1,9 +1,19 @@
 using System;
+using System.Runtime.InteropServices.Marshalling;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment a1 = new Assignment("Mason Carver", "Mtn Dew-ology");
+        Console.WriteLine(a1.GetSummary());
+
+        MathAssignment a2 = new MathAssignment("Mason Carver", "Math... gross", "69", "4-20");
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetHomeworkList());
+
+        WritingAssignment a3 = new WritingAssignment("Mason Carver", "I LOVE WRITING!!!", "The Day I Became A Snail... and Ate a Worm.");
+        Console.WriteLine(a3.GetSummary());
+        Console.WriteLine(a3.GetWritingInformation());
     }
 }
